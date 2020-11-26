5bf95e3ab6f2d0b037598aae910627eb9e33851d
Alibay74 <aitov.74@list.ru>
2020-11-25 19:13:56 -0500

    First added

---
+++ src/First.java
@@ -0,0 +1,18 @@
+package KZ;
+
+
+
+public class First {
+public static void main(String[] args) {
+
+System.out.println("Hello world IT ");
+System.out.println("Hallo Alibay Aitov");
+System.out.println("paprmagmatica ohoho");
+}
+}
+
+
+
+
+
+
